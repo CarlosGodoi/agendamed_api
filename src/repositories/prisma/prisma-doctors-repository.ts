1a9b22e13696581254ab350ebed38bfe3c0c1b41
carlosgodoi <edu-godoi@hotmail.com>
2025-04-11 15:03:11 -0300

    refactor: Including name at in specialties

--- src/repositories/prisma/prisma-doctors-repository.ts
+++ src/repositories/prisma/prisma-doctors-repository.ts
@@ -5,113 +5,122 @@ import { prisma } from "@/lib/prisma";
 import { AppError } from "@/utils/errors/AppError";
 
 const Pagination = (skip: number, take: number) => {
-    const calcSkip = (skip - 1) * take
+  const calcSkip = (skip - 1) * take;
 
-    const pagination = {
-        skip: calcSkip < 0 ? 0 : calcSkip,
-        take: Number(take)
-    }
+  const pagination = {
+    skip: calcSkip < 0 ? 0 : calcSkip,
+    take: Number(take),
+  };
 
-    return pagination
-}
+  return pagination;
+};
 
 interface GetAllDoctorsParams {
-    skip?: number;
-    take?: number;
+  skip?: number;
+  take?: number;
 }
 
 export interface IDoctorsParamsGetAll extends IPagination {
-    doctors: Doctor[];
-    total: number;
-    totalPage?: number;
+  doctors: Doctor[];
+  total: number;
+  totalPage?: number;
 }
 
 export class PrismaDoctorsRepository implements DoctorsRepository {
-    async create(data: Prisma.DoctorCreateInput) {
-        const doctor = await prisma.doctor.create({
-            data
-        })
+  async create(data: Prisma.DoctorCreateInput) {
+    const doctor = await prisma.doctor.create({
+      data,
+    });
 
-        return doctor
+    return doctor;
+  }
+
+  async getAll({
+    skip,
+    take,
+  }: GetAllDoctorsParams): Promise<IDoctorsParamsGetAll> {
+    let pagination: IPagination = {};
+    let where: Prisma.DoctorWhereInput = {};
+
+    if (skip && take) {
+      pagination = Pagination(skip, take);
     }
 
-    async getAll({ skip, take }: GetAllDoctorsParams): Promise<IDoctorsParamsGetAll> {
-        let pagination: IPagination = {}
-        let where: Prisma.DoctorWhereInput = {}
+    const doctors = await prisma.doctor.findMany({
+      ...pagination,
+      where,
+      include: {
+        specialty: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
 
-        if (skip && take) {
-            pagination = Pagination(skip, take);
-        }
+    const total = await prisma.doctor.count({ where });
+    const totalPage = take ? Math.ceil(total / take) : total;
 
-        const doctors = await prisma.doctor.findMany({
-            ...pagination,
-            where
-        })
+    return {
+      doctors,
+      total,
+      ...(pagination.take && { totalPage }),
+    };
+  }
 
-        const total = await prisma.doctor.count({ where });
-        const totalPage = take ? Math.ceil(total / take) : total;
+  async getById(id: string) {
+    const doctor = await prisma.doctor.findFirst({
+      where: {
+        id,
+      },
+    });
+    return doctor;
+  }
 
-        return {
-            doctors,
-            total,
-            ...(pagination.take && { totalPage })
-        };
+  async findByName(name: string) {
+    const doctor = await prisma.doctor.findFirst({
+      where: {
+        name,
+      },
+    });
+
+    return doctor;
+  }
+
+  async findById(id: string) {
+    const doctor = await prisma.doctor.findFirst({
+      where: {
+        id,
+      },
+    });
+    return doctor;
+  }
+
+  async findByCpf(cpf: string) {
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        cpf,
+      },
+    });
+
+    return doctor;
+  }
+
+  async delete(id: string) {
+    const doctor = await prisma.doctor.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!doctor) {
+      throw new AppError("error", "Doctor not found.");
     }
 
-    async getById(id: string) {
-        const doctor = await prisma.doctor.findFirst({
-            where: {
-                id
-            }
-        })
-        return doctor
-    }
-
-    async findByName(name: string) {
-        const doctor = await prisma.doctor.findFirst({
-            where: {
-                name
-            }
-        })
-
-        return doctor
-    }
-
-    async findById(id: string) {
-        const doctor = await prisma.doctor.findFirst({
-            where: {
-                id
-            }
-        })
-        return doctor
-    }
-
-    async findByCpf(cpf: string) {
-        const doctor = await prisma.doctor.findUnique({
-            where: {
-                cpf
-            }
-        })
-
-        return doctor
-    }
-
-    async delete(id: string) {
-        const doctor = await prisma.doctor.findUnique({
-            where: {
-                id
-            }
-        })
-
-        if (!doctor) {
-            throw new AppError('error', 'Doctor not found.')
-        }
-
-        await prisma.doctor.delete({
-            where: {
-                id
-            }
-        })
-    }
-
+    await prisma.doctor.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
